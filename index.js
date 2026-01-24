c0b458d108eb0dded315d07b79859ecd4dd09336
Daniel Lai <laidaniel759@gmail.com>
2026-01-24 12:43:46 +0800

    connect to database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
+import dotenv from 'dotenv'
 import { fileURLToPath } from 'url'
 import { Pool } from 'pg'
 
@@ -12,6 +13,26 @@ const __dirname = path.dirname(__filename)
 
 app.use(express.json())
 app.use(cors())
+dotenv.config()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+  },
+});
+
+async function getPostgresVersion() {
+  const client = await pool.connect();
+  try {
+    const response = await client.query("SELECT version()");
+    console.log(response.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPostgresVersion();
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
